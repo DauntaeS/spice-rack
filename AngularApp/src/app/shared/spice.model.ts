063f2330b05cd33778ae78dba3f7da1d57c3eafb
Dauntae <dauntae9@gmail.com>
2023-11-09 17:31:24 -0600

    simple form setup using Angular

---
+++ AngularApp/src/app/shared/spice.model.ts
@@ -0,0 +1,9 @@
+export class Spice {
+    _id!: string;
+    name!: string;
+    qauntity!: number;
+    amount!: string;
+    description!: string;
+    dateAdded!: string;
+    expDate!: string;
+}
