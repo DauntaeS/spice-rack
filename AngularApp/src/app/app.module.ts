8647dc32552ef303c55ea4cf39d2e7cf166669d0
Dauntae <dauntae9@gmail.com>
2023-11-11 21:19:51 -0600

    Form logic added

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { SpiceComponent } from './spice/spice.component';
   imports: [
     FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
